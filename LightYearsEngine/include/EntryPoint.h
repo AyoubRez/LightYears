a10e50dbade6e30bb6d123dbed5322804260b67e
AyoubREZZOUK <rezzouk.ayouble@gmail.com>
2026-02-22 20:40:55 +0100

    chore: Separate Engine and Game targets,

---
+++ LightYearsEngine/include/EntryPoint.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+
+namespace ly
+{
+    class Application;
+}
+
+extern ly::Application* get_application();
---
+++ LightYearsEngine/src/EntryPoint.cpp
@@ -0,0 +1,9 @@
+﻿#include "EntryPoint.h"
+#include "framework/Application.h"
+
+int main(int argc, char* argv[])
+{
+    ly::Application* app = get_application();
+    app->Run();
+    delete app;
+}
