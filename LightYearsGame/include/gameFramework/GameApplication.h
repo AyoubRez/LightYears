d1d33b6dff93d2a0c728638c325ff1d93235fe68
AyoubREZZOUK <rezzouk.ayouble@gmail.com>
2026-02-22 21:17:14 +0100

    feat: separate light engine from game

---
+++ LightYearsGame/include/gameFramework/GameApplication.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <framework/Application.h>
+
+namespace ly
+{
+    class GameApplication : public Application
+    {
+    };
+}
---
+++ LightYearsGame/src/gameFramework/GameApplication.cpp
@@ -0,0 +1,12 @@
+#include "gameFramework/GameApplication.h"
+
+
+ly::Application* get_application()
+{
+    return new ly::GameApplication();
+}
+
+namespace ly
+{
+    
+}
